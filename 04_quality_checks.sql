ffa6198e272cdf8c5f66e0abed5df184f32d1e80
Mateusz <mateuszhorabik31@gmail.com>
2026-05-17 13:54:18 +0200

    Check nulls is orders table

--- 04_quality_checks.sql
+++ 04_quality_checks.sql
@@ -1,6 +1,4 @@
-use data_quality_project
-go
-select * from customers 
-where email is null
-select * from customers 
-where city is null
+select * from orders
+where total_amount is null	
+select * from orders
+where order_date is null	
